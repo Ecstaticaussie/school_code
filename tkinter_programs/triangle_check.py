59703cc3d2c82d125ed82a57038a4fbb4322458d
Ecstaticaussie <108424925+Ecstaticaussie@users.noreply.github.com>
2023-04-25 13:20:18 +0100

    -

--- tkinter_programs/triangle_check.py
+++ tkinter_programs/triangle_check.py
@@ -3,16 +3,16 @@ from tkinter.ttk import Label, Button, Entry
 
 #Used to show messages easier
 def show_message(txt):
-    message_label = Label(root, text=txt)
+    message_label = Label(root, text=txt,)
     message_label.grid(row=5, column=1)
 
 #This is done to check if the inputs and sides are correct
 def check_sides(event):
     #Checks to see if all entry boxes have an input
-    if not side1_entry.get() or not side1_entry.get() or not side1_entry.get():
-        show_message("All entry boxes need to be filled")
+    if side1_entry.get() == "" or side2_entry.get() == "" or side3_entry.get() == "":
+        show_message("All entry boxes need to be filled.")
     #Checks to see if the entry boxes has any 0's
-    elif side1_entry.get() == 0 or side1_entry.get() == 0 or side1_entry.get() == 0:
+    elif side1_entry.get() == 0 or side2_entry.get() == 0 or side3_entry.get() == 0:
         show_message("Don't input 0's.")
     #Checks to see if the inputs are integers (tried to make it using a minimal amount of lines)
     elif not(side1_entry.get().isdigit() and side2_entry.get().isdigit() and side3_entry.get().isdigit()):
