8d8c3ac8b3a9477b2b70e761e090cfd3a1edabc1
Ecstaticaussie <108424925+Ecstaticaussie@users.noreply.github.com>
2023-05-10 11:55:02 +0100

    Updated the linked list and node classes

--- in_class/linked_lists.py
+++ in_class/linked_lists.py
@@ -1,14 +1,14 @@
 #creation of a node (element in a linekd list)
 
 class Node:
-    def __init__(self, left_pointer=None, right_pointer=None, value=None, at_head=None, at_tail=None):
+    def __init__(self, left_pointer=None, right_pointer=None, value=None):
         self.value = value
         self.back_pointer = left_pointer
         self.forward_pointer = right_pointer
 
 #The linked list class
 class Linked_list:
-    def __init__(self, size):
+    def __init__(self, size, head=None, tail=None):
         self.size = size
-        self.head = None
-        self.tail = None
+        self.head = head
+        self.tail = tail
