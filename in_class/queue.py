cb395f5f97bdf9fbfcddc76f803b740727308233
Ecstaticaussie <108424925+Ecstaticaussie@users.noreply.github.com>
2023-05-16 13:25:35 +0100

    added circular queue class

--- in_class/queue.py
+++ in_class/queue.py
@@ -5,7 +5,7 @@ class Queue:
         self.max_size = max_size
         self.current_size = 0
         self.queue = [None] * max_size
-        self.front_pointer = 0
+        self.back_pointer = 0
 
     #Checks if it is empty
     def is_empty(self):
@@ -27,17 +27,30 @@ class Queue:
         for i in range(len(self.queue)):
             if self.queue[i] != None:
                 self.queue[i] = elem
-                self.front_pointer += 1
+                self.back_pointer += 1
+                self.current_size += 1
 
     #Removing elements -> Dequeue
-    def dequeue(self, elem):
+    def dequeue(self):
         #Checks if the queue is empty
         if self.isempty(): print("The queue is empty"); return
         #Checks if the element is there -> Removes it
         for i in reversed(range(len(self.queue))):
             if self.queue[i] != None:
                 self.queue[i] = None
-                self.front_pointer -= 1
+                self.back_pointer -= 1
+                self.current_size -= 1
 
+#creation of the circular queue class -> inheritance of the Queue Class
+class C_Queue(Queue):
+    def __init__(self, max_size):
+        super.__init__(max_size)
 
+    #Polymorphism of the enqueue and dequeue methods
+    def enqueue(self):
+        #Checks if it is full
+        if self.isfull(): print("The queue is full."); return
 
+    def dequeue(self):
+        #Checks if the queue is empty
+        if self.isempty(): print("The queue is empty"); return
