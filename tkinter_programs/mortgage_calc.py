be876366bb9ad503bc06ada85a90a6b5a81aef68
Ecstaticaussie <108424925+Ecstaticaussie@users.noreply.github.com>
2023-05-18 19:39:26 +0100

    User can input the values needed to calculate mortgage

--- tkinter_programs/mortgage_calc.py
+++ tkinter_programs/mortgage_calc.py
@@ -1,25 +1,32 @@
-from customtkinter import CTk, CTkLabel, CTkEntry, CTkRadioButton
-from tkinter import StringVar
+from customtkinter import CTk, CTkLabel, CTkEntry
 """
 Need the user to input the number of terms - integers
 Get the starting amount -> A float to 2d.p.
-Get the annual interest rate from the user -> get a percentage and make it a float based on the regular intervals
-Calculate the time for the user to pay the loan back -> Do it in terms of the regular intervals
-
-Extension:
-Allow user to change the interval (e.g. monthly)
+Get the annual interest rate from the user -> get a percentage and make it a float based on years
+Calculate the time for the user to pay the loan back -> Do it in terms of years
 """
 
-#Where the root class is made
-class Root(CTk):
-    def __init__(self):
-        super().__init__()
+#The info about the window 
+root = CTk()
+root.title("Mortgage calculator")
+root.geometry("400x400")
 
-        #Window information
-        self.title("Mortgage Calculator ")
-        self.geometry("400x400")
+starting_amount_label = CTkLabel(root, text="Starting Amount")
+starting_amount_label.place(x=20, y=10)
 
-        #Entry box to allow the user to choose the regular interval
-        self.regular_intervals_Radio = CTkRadioButton(self, )
-root = Root()
+starting_amount_entry = CTkEntry(root)
+starting_amount_entry.place(x=130, y=10)
+
+annual_interest_label = CTkLabel(root, text="Annual_interest")
+annual_interest_label.place(x=20, y=50)
+
+annual_interest_entry = CTkEntry(root, placeholder_text="Give as a Percentage %")
+annual_interest_entry.place(x=130, y=50)
+
+terms_label = CTkLabel(root, text="Starting Amount")
+terms_label.place(x=20, y=90)
+
+terms_entry = CTkEntry(root, placeholder_text="Give it in Years")
+terms_entry.place(x=130, y=90)
+
 root.mainloop()
