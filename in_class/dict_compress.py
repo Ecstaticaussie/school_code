95533f614ffb274f11e2f3115c3d58eb77dc0c95
Ecstaticaussie <108424925+Ecstaticaussie@users.noreply.github.com>
2023-06-14 18:23:46 +0100

    dcit_comp_ works

--- in_class/dict_compress.py
+++ in_class/dict_compress.py
@@ -1,11 +1,12 @@
 from math import log2, ceil
 
-remove_chars = ",.?/:;!&()"
+remove_chars = ",.?/:;!&()".strip()
 text = "".join([char for char in [*"What a start to this game. Belief from Morocco that they can take the game to Croatia. This is constant pressure from Morocco!"] if char not in ",.?/:;!&()"])
 
 number_of_chars = len(text)
 text_list = text.split()
 unique_words = len(set(text_list))
+print(unique_words)
 
 uncompressed_size = number_of_chars * 16
 compressed_size = unique_words * ceil(log2(unique_words))
